d4a691e32d42171d9a632e4ad1c7acc83ccbaec7
Kishita Sharma <kishitasharma0@gmail.com>
2025-04-04 16:58:25 +0530

    Updated HTML and JS files

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ async function checkWeather(city) {
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
     
         if(data.weather[0].main == "Clouds"){
-            weatherIcon.src = "clouds.png";
+            weatherIcon.src = "imgs/clouds.png";
         }
         else if(data.weather[0].main == "Clear"){
-            weatherIcon.src = "clear.png";
+            weatherIcon.src = "imgs/clear.png";
         }
         else if(data.weather[0].main == "Rain"){
-            weatherIcon.src = "rain.png";
+            weatherIcon.src = "imgs/rain.png";
         }
         else if(data.weather[0].main == "Drizzle"){
-            weatherIcon.src = "drizzle.png";
+            weatherIcon.src = "imgs/drizzle.png";
         }
         else if(data.weather[0].main == "Mist"){
-            weatherIcon.src = "mist.png";
+            weatherIcon.src = "imgs/mist.png";
         }
     
         document.querySelector(".weather").style.display = "block";
